1711bbcdbbf3910862627f88c7b7140961bb3f92
NoSequel <nosequel@protonmail.com>
2021-09-08 04:32:29 +0200

    add base spigot implementation

--- shared/src/main/java/io/github/nosequel/core/shared/Pump.java
+++ shared/src/main/java/io/github/nosequel/core/shared/Pump.java
@@ -1,7 +1,15 @@
 package io.github.nosequel.core.shared;
 
+import io.github.nosequel.core.shared.rank.RankHandler;
+import io.github.nosequel.storage.mongo.MongoStorageHandler;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class Pump {
 
-
+    private final MongoStorageHandler storageHandler;
+    private final RankHandler rankHandler;
 
 }
---
+++ spigot/src/main/java/io/github/nosequel/core/spigot/PumpJavaPlugin.java
@@ -0,0 +1,25 @@
+package io.github.nosequel.core.spigot;
+
+import io.github.nosequel.core.shared.Pump;
+import io.github.nosequel.core.shared.rank.RankHandler;
+import io.github.nosequel.storage.mongo.MongoStorageHandler;
+import io.github.nosequel.storage.mongo.settings.MongoSettings;
+import io.github.nosequel.storage.mongo.settings.impl.NoAuthMongoSettings;
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public class PumpJavaPlugin extends JavaPlugin {
+
+    private Pump pump;
+
+    @Override
+    public void onEnable() {
+        final MongoSettings settings = new NoAuthMongoSettings("127.0.0.1", 27017, "pump");
+        final MongoStorageHandler storageHandler = new MongoStorageHandler(settings);
+
+        this.pump = Pump.builder()
+                .storageHandler(storageHandler)
+                .rankHandler(new RankHandler(storageHandler)).build();
+    }
+}
